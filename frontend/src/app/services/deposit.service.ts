2aacacf027c4a08f0e0e081354090c2a0b369e99
Roberts Zustars <robert.zustars@gmail.com>
2025-04-13 21:03:38 +0200

    Update configuration to support both local and Docker environments
    - Refactored backend datasource config with application-local.properties
    - Updated frontend to use environment.ts for API base URL
    - Adjusted Dockerfile and docker-compose setup
    - Proxy config for Angular added to route /api correctly

--- frontend/src/app/services/deposit.service.ts
+++ frontend/src/app/services/deposit.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DepositRequest } from '../models/deposit-request.model';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepositService {
 
-  private apiUrl = '/api/deposits';
+  private apiUrl = `${environment.apiUrl}/deposits`;
 
   constructor(private http: HttpClient) {}
 
