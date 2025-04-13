9deec4104fa51ca9d965b8b1018462f112478f3f
Roberts Zustars <robert.zustars@gmail.com>
2025-04-13 18:58:49 +0200

    Add Dockerfiles for frontend and backend with docker-compose setup

--- frontend/src/app/services/deposit.service.ts
+++ frontend/src/app/services/deposit.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class DepositService {
 
-  private apiUrl = '/deposits';
+  private apiUrl = '/api/deposits';
 
   constructor(private http: HttpClient) {}
 
